71bce0b483468f99b5a81ed7cc12c634a2b57277
jamiroZ <jamiro.zuniga@est.fi.uncoma.edu.ar>
2024-10-03 00:23:21 -0300

    tabla parcialmente hecha hasta la fila 3 y css implementado

--- interaccion/interaccion.js
+++ interaccion/interaccion.js
@@ -1,33 +1,11 @@
-const elements = document.querySelectorAll('.element');
+let contador = 0;
 
-elements.forEach((element) => {
-  element.addEventListener('click', () => {
-    const elementInfo = getElementInfo(element.id);
-    displayElementInfo(elementInfo);
-  });
-});
-
-function getElementInfo(elementId) {
-  // Return the information for the element with the given ID
-  // This can be a JSON object or a string containing the information
-  // For example:
-  return {
-    name: 'Hydrogen',
-    symbol: 'H',
-    atomicNumber: 1,
-    atomicMass: 1.00794,
-  };
-}
-
-function displayElementInfo(elementInfo) {
-  // Display the element information in a modal window or a tooltip
-  // For example:
-  const modal = document.getElementById('modal');
-  modal.innerHTML = `
-    <h2>${elementInfo.name}</h2>
-    <p>Symbol: ${elementInfo.symbol}</p>
-    <p>Atomic Number: ${elementInfo.atomicNumber}</p>
-    <p>Atomic Mass: ${elementInfo.atomicMass}</p>
-  `;
-  modal.style.display = 'block';
+function ocultarBotones() {
+    contador++;
+    if (contador === 2) {
+        const botones = document.querySelectorAll('button');
+        botones.forEach(boton => {
+            boton.style.display = 'none'; // Oculta todos los botones
+        });
+    }
 }
